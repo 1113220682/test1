157fc3153936f687b0b7d2e0b5b14220fb8c934f
liuxinboo <liuxinboo@163.com>
2018-01-25 22:52:32 +0800

    first commit

---
+++ js/common.js
@@ -0,0 +1,345 @@
+$(function () {
+ /*   $(function () {
+        $('.to011').click(function () {
+            $('#iframeCtrl').attr('src','chapter01/chapterBase011.html');
+        });
+        $('.to012').click(function () {
+            $('#iframeCtrl').attr('src','chapter01/chapterBase012.html');
+        });
+        $('.to021').click(function () {
+            $('#iframeCtrl').attr('src','chapter02/chapterBase021.html');
+        });
+        $('.to031').click(function () {
+            $('#iframeCtrl').attr('src','chapter03/chapterBase031.html');
+        });
+        $('.to032').click(function () {
+            $('#iframeCtrl').attr('src','chapter03/chapterBase032.html');
+        });
+        $('.to033').click(function () {
+            $('#iframeCtrl').attr('src','chapter03/chapterBase033.html');
+        });
+        $('.to041').click(function () {
+            $('#iframeCtrl').attr('src','chapter04/chapterBase041.html');
+        });
+        $('.to042').click(function () {
+            $('#iframeCtrl').attr('src','chapter04/chapterBase042.html');
+        });
+        $('.to043').click(function () {
+            $('#iframeCtrl').attr('src','chapter04/chapterBase043.html');
+        });
+        $('.to051').click(function () {
+            $('#iframeCtrl').attr('src','chapter05/chapterBase051.html');
+        });
+        $('.to052').click(function () {
+            $('#iframeCtrl').attr('src','chapter05/chapterBase052.html');
+        });
+        $('.to061').click(function () {
+            $('#iframeCtrl').attr('src','chapter06/chapterBase061.html');
+        });
+        $('.to062').click(function () {
+            $('#iframeCtrl').attr('src','chapter06/chapterBase062.html');
+        });
+        $('.to063').click(function () {
+            $('#iframeCtrl').attr('src','chapter06/chapterBase063.html');
+        });
+        $('.to064').click(function () {
+            $('#iframeCtrl').attr('src','chapter06/chapterBase064.html');
+        });
+        $('.to065').click(function () {
+            $('#iframeCtrl').attr('src','chapter06/chapterBase065.html');
+        });
+        $('.to071').click(function () {
+            $('#iframeCtrl').attr('src','chapter07/chapterBase071.html');
+        });
+        $('.to072').click(function () {
+            $('#iframeCtrl').attr('src','chapter07/chapterBase072.html');
+        });
+        $('.to081').click(function () {
+            $('#iframeCtrl').attr('src','chapter08/chapterBase081.html');
+        });
+        $('.to082').click(function () {
+            $('#iframeCtrl').attr('src','chapter08/chapterBase082.html');
+        });
+        $('.to091').click(function () {
+            $('#iframeCtrl').attr('src','chapter09/chapterBase091.html');
+        });
+        $('.to0100').click(function () {
+            $('#iframeCtrl').attr('src','chapter10/chapterBase0100.html');
+        });
+        $('.to0111').click(function () {
+            $('#iframeCtrl').attr('src','chapter11/chapterBase0111.html');
+        });
+        $('.to0112').click(function () {
+            $('#iframeCtrl').attr('src','chapter12/chapterBase0112.html');
+        });
+        $('.to0113').click(function () {
+            $('#iframeCtrl').attr('src','chapter13/chapterBase0113.html');
+        });
+    })*/
+
+    // $('.to011').click(function () {
+    //     window.location.href='chapterBase011.html';
+    // });
+    // $('.to012').click(function () {
+    //     window.location.href='chapterBase012.html';
+    // });
+    // $('.to011').click(function () {
+    //     window.location.href='chapterBase011.html';
+    // });
+    // $('.to011').click(function () {
+    //     window.location.href='chapterBase011.html';
+    // });
+    // $('.to011').click(function () {
+    //     window.location.href='chapterBase011.html';
+    // });
+    // $('.to011').click(function () {
+    //     window.location.href='chapterBase011.html';
+    // });
+    // $('.to011').click(function () {
+    //     window.location.href='chapterBase011.html';
+    // });
+    // $('.to011').click(function () {
+    //     window.location.href='chapterBase011.html';
+    // });
+    // $('.to011').click(function () {
+    //     window.location.href='chapterBase011.html';
+    // });
+    // $('.to011').click(function () {
+    //     window.location.href='chapterBase011.html';
+    // });
+    // $('.to012').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter01/chapterBase012.html');
+    // });
+    // $('.to021').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter02/chapterBase021.html');
+    // });
+    // $('.to031').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter03/chapterBase031.html');
+    // });
+    // $('.to032').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter03/chapterBase032.html');
+    // });
+    // $('.to033').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter03/chapterBase033.html');
+    // });
+    // $('.to041').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter04/chapterBase041.html');
+    // });
+    // $('.to042').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter04/chapterBase042.html');
+    // });
+    // $('.to043').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter04/chapterBase043.html');
+    // });
+    // $('.to051').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter05/chapterBase051.html');
+    // });
+    // $('.to052').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter05/chapterBase052.html');
+    // });
+    // $('.to061').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter06/chapterBase061.html');
+    // });
+    // $('.to062').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter06/chapterBase062.html');
+    // });
+    // $('.to063').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter06/chapterBase063.html');
+    // });
+    // $('.to064').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter06/chapterBase064.html');
+    // });
+    // $('.to065').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter06/chapterBase065.html');
+    // });
+    // $('.to071').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter07/chapterBase071.html');
+    // });
+    // $('.to072').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter07/chapterBase072.html');
+    // });
+    // $('.to081').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter08/chapterBase081.html');
+    // });
+    // $('.to082').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter08/chapterBase082.html');
+    // });
+    // $('.to091').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter09/chapterBase091.html');
+    // });
+    // $('.to0100').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter10/chapterBase0100.html');
+    // });
+    // $('.to0111').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter11/chapterBase0111.html');
+    // });
+    // $('.to0112').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter12/chapterBase0112.html');
+    // });
+    // $('.to0113').click(function () {
+    //     $('#iframeCtrl').attr('src','chapter13/chapterBase0113.html');
+    // });
+    $(document).ready(function(){
+        $("#faqdiv1").on("click",".ivu-select-item",getVal);
+    })
+
+function getVal(){
+    var value =  $(this).html();
+    $(".pleace").html(value);
+   /* $("ivu-select-dropdown").css("display","none");*/
+}
+
+    var nav = $(".side"), st; //导航条最外围的id
+    // $(window).scroll(function () {
+    //     st = Math.max(document.body.scrollTop || document.documentElement.scrollTop); //获取到滚动条距上边的距离
+    //     if (st > 100 || st == 100) {
+    //         nav.css({"position": "fixed", "top": "88px", 'z-index': '9'});
+    //     } else if (st < 87 || st == 87) {
+    //         nav.css({"position":"absolute","top": "88px"});
+    //     }
+    // });
+    var nHeight=$('.tableShow').height();
+    $('.content').css({'height':nHeight});
+    $('.menu-title').each(function () {
+        $(this).click(function () {
+            $(this).siblings().toggleClass('displayCtrl');
+            console.log($(this).parent().siblings().find('.menu-sublist').addClass('displayCtrl'))
+        });
+        $(this).parent().click(function () {
+            // $(this).addClass('onDd').siblings().removeClass('onDd');
+        })
+    });
+    $('.menu-subtitle').click(function () {
+        $(this).addClass('onColor').siblings().removeClass('onColor');
+    })
+
+    // $('.menu-subtitle')
+});
+
+$(function(){
+    $('.yes1').click(function(){
+        $('.hide1').css('display','none')
+    })
+    $('.no1').click(function(){
+        $('.hide1').css('display','block')
+    })
+    $('.yes2').click(function(){
+        $('.hide2').css('display','block')
+    })
+    $('.no2').click(function(){
+        $('.hide2').css('display','none')
+    })
+});
+
+
+$(function() {
+    $('#preserve').click(function () {
+        $('.save').css('display','block')
+    })
+
+});
+
+
+$(function() {
+    $(".but").click(function() {
+        $("#faqbg").css({
+            display : "block",
+            height : $(document).height()
+        });
+        var yscroll = document.documentElement.scrollTop;
+        $("#faqdiv").css("top", "100px");
+        $("#faqdiv").css("display", "block");
+        document.documentElement.scrollTop = 0;
+    });
+
+    $(".close").click(function() {
+        $("#faqbg").css("display", "none");
+        $("#faqdiv").css("display", "none");
+        $('#textone').val(null);
+    });
+})
+
+$(function() {
+    $(".add").click(function() {
+        $("#faqbg").css({
+            display : "block",
+            height : $(document).height()
+        });
+        var yscroll = document.documentElement.scrollTop;
+        $("#faqdiv1").css("top", "100px");
+        $("#faqdiv1").css("display", "block");
+        document.documentElement.scrollTop = 0;
+    });
+
+    $(".close").click(function() {
+        $("#faqbg").css("display", "none");
+        $("#faqdiv1").css("display", "none");
+        $('#textone').val(null);
+    });
+})
+
+
+$(function() {
+    $(".pleace").click(function() {
+        $(".serial").css("display","block");
+    });
+})
+
+$(function() {
+    $(".a-n").click(function() {
+        $("#faqbg").css({
+            display : "block",
+            height : $(document).height()
+        });
+        var yscroll = document.documentElement.scrollTop;
+        $("#faqdiv2").css("top", "100px");
+        $("#faqdiv2").css("display", "block");
+        document.documentElement.scrollTop = 0;
+    });
+
+    $(".close").click(function() {
+        $("#faqbg").css("display", "none");
+        $("#faqdiv2").css("display", "none");
+        $('#textone').val(null);
+    });
+})
+
+$(function() {
+    $(".a-n1").click(function() {
+        $("#faqbg").css({
+            display : "block",
+            height : $(document).height()
+        });
+        var yscroll = document.documentElement.scrollTop;
+        $("#faqdiv3").css("top", "100px");
+        $("#faqdiv3").css("display", "block");
+        document.documentElement.scrollTop = 0;
+    });
+
+    $(".close").click(function() {
+        $("#faqbg").css("display", "none");
+        $("#faqdiv3").css("display", "none");
+        $('#textone').val(null);
+    });
+})
+
+$(function() {
+    $(".pt").click(function() {
+        $("#faqbg").css({
+            display : "block",
+            height : $(document).height()
+        });
+        var yscroll = document.documentElement.scrollTop;
+        $("#faqdiv4").css("top", "100px");
+        $("#faqdiv4").css("display", "block");
+        document.documentElement.scrollTop = 0;
+    });
+
+    $(".close").click(function() {
+        $("#faqbg").css("display", "none");
+        $("#faqdiv4").css("display", "none");
+        $('#textone').val(null);
+    });
+})
+
+
